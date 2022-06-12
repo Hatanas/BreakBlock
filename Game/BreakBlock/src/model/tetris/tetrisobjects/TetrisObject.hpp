4e5f01f96f12751e53a123d64fc264287a832fc3
Jumpaku <Jumpaku@users.noreply.github.com>
2022-06-12 10:02:45 +0900

    feat: add TetrisObject

--- Game/BreakBlock/src/model/tetris/tetrisobjects/TetrisObject.hpp
+++ Game/BreakBlock/src/model/tetris/tetrisobjects/TetrisObject.hpp
@@ -8,6 +8,51 @@
 #ifndef TetrisObject_hpp
 #define TetrisObject_hpp
 
-#include <stdio.h>
+#include <unordered_map>
 
+namespace breakblock::model::tetris::tetrisobjects {
+
+enum BlockType
+{
+    Default,
+};
+
+struct CellIndex
+{
+    int row;
+    int column;
+    struct Hasher
+    {
+        int operator()(CellIndex const &cellIndex) const
+        {
+            return cellIndex.row * 31 + cellIndex.column;
+        }
+    };
+    bool operator==(CellIndex const&other)const{
+        return row == other.row && column == other.column;
+    }
+};
+
+struct TetrisObject
+{
+    std::unordered_map<CellIndex, BlockType, CellIndex::Hasher> blocks;
+    bool intersects(TetrisObject const&other)const{
+        TetrisObject out;
+        for (auto const&[cellIndex, blockType] : blocks) {
+            if(other.blocks.find(cellIndex) != other.blocks.end()){
+                out.blocks.insert({cellIndex, blockType});
+            }
+        }
+        return out.blocks.size() > 0;
+    }
+    TetrisObject merge(TetrisObject const&other)const{
+        TetrisObject out = *this;
+        for (auto const&[cellIndex, blockType] : blocks) {
+            out.blocks.insert({cellIndex, blockType});
+        }
+        return out;
+    }
+    
+};
+}
 #endif /* TetrisObject_hpp */
